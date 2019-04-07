630043fc627896de5881be0361c1564334e30c17
hy <bb.kiera@gmail.com>
2019-04-07 22:30:15 +0900

    simple shell

--- week4/shell/simple_shell.c
+++ week4/shell/simple_shell.c
@@ -7,20 +7,57 @@
 #define MAX_LEN 100 /* The maximum length command */
 
 int main(void) {
-  char *args[MAX_LEN / 2 + 1]; /* command line arguments */
-  int should_run = 1;          /* flag to determine when to exit program */
-  int background = 0;
+		char *args[MAX_LEN / 2 + 1]; /* command line arguments */
+		int should_run = 1;          /* flag to determine when to exit program */
+		int background = 0;
 
-  while (should_run) {
-    printf("my_shell> ");
-    fflush(stdout);
-    /**
-     * 표준입출력으로부터 문자열을 입력 받은 후:
-     * (1) fork()를 통해 자식 프로세스를 생성
-     * (2) 자식 프로세스가 execvp()를 호출하도록 할 것
-     * (3) 만약 입력받은 문자에 &가 포함되어 있으면,
-     *     부모 프로세스는 wait() 호출
-     */
-  }
-  return 0;
+		char *input;
+		int status;
+
+while (should_run) {
+		printf("my_shell> ");
+		fflush(stdout);
+
+		input = (char*)malloc(MAX_LEN*sizeof(char));
+		fgets(input, MAX_LEN, stdin);
+
+		int i = 0;
+		args[i] = strtok(input, " \n");
+		while (args[i] != NULL) {
+				i++;
+				args[i] = strtok(NULL, " \n");
+		}
+		if (args[0] == NULL)
+				goto no_input;
+
+		if (strcmp(args[0], "exit") == 0) {
+				break;
+		}
+    	
+		background = 0;
+		if (strcmp(args[i-1], "&") == 0) {
+				args[i - 1] = NULL;
+				background = 1;
+		}
+		
+		pid_t pid = fork();
+		if (pid < 0) {
+				perror("Fork error");
+				exit(0);
+		}
+
+		if (pid == 0) {
+				execvp(args[0], args);
+		}
+
+		if (pid > 0) {
+				if (!background) {
+						waitpid(pid, NULL, 0);
+				}
+		}		
+		no_input:
+			continue;
+	}
+	free(input);
+	return 0;
 }
